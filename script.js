0058580df323632a02f47d3957639473fd8c8ac1
josevga2016 <116651547+josevga2016@users.noreply.github.com>
2023-05-22 17:25:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+
+
+function revisionTexto (texto) {
+    
+    let caracteres_especiales = /[~!@#$%^&*()_+|}{[\]\\\/?><:"`;.,áéíóúàèìòù']/g;
+    let mayusculas = /[A-Z]/g;  
+    let vacio="";  
+      
+    if(texto.match(mayusculas)||texto.match(caracteres_especiales)){
+        alert("No se aceptan caracteres especiales ni mayusculas");
+        return true; 
+    }else if(texto==vacio){
+       alert("Aun no has ingresado un mensaje para encriptar");
+        return true;
+   }else {
+        return false;
+    }
+}
+
+
+let btnEncriptar = document.querySelector("#btn-encriptar");
+
+btnEncriptar.addEventListener("click",function ()  {
+    let textInput = document.querySelector("#input-texto").value;
+    let textoIngresado = textInput;
+   
+    if (revisionTexto (textoIngresado) == false) {       
+        let Encriptado = encriptar(textoIngresado);
+        let resultado = document.querySelector("#msg");
+        resultado.value = Encriptado;
+    } else {        
+        textInput = "";     
+     
+    }
+               
+})
+
+
+const reglas = { "e":"enter","i":"imes","a":"ai","o":"ober","u":"ufat"};
+
+function encriptar (textoIngresado) {
+    let Encriptado = "";
+    for (const obj in reglas) {
+        Encriptado = textoIngresado.replaceAll(obj,reglas[obj]);
+        textoIngresado = Encriptado;        
+    }
+    return (Encriptado);
+}
+
+
+let btnCopiar = document.querySelector("#btn-copy");
+
+btnCopiar.addEventListener("click",function(){        
+    let Copiado = document.querySelector("#msg").value;
+    navigator.clipboard.writeText(Copiado);
+    document.querySelector("#input-texto").value="";
+
+});
+
+let btnDesencriptar = document.querySelector("#btn-desencriptar");
+
+btnDesencriptar.addEventListener("click", function(){
+    let textoIngresado = document.querySelector("#input-texto").value;
+    let Desencriptado = desencriptar(textoIngresado);
+
+    let resultado = document.querySelector("#msg");
+    resultado.value = Desencriptado;
+})
+
+
+
+function desencriptar (textoIngresado) {
+    let Encriptado = "";
+    for (const obj in reglas) {
+        Encriptado = textoIngresado.replaceAll(reglas[obj],obj);
+        textoIngresado = Encriptado;        
+    }
+    return (Encriptado);
+}
+
+
+
+
+
+
+
+
+
